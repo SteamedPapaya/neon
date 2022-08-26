e2863e7264a32eb5436098296bb9bd7d6e66b1da
ryuu <ryuu.public@gmail.com>
2022-08-26 22:33:12 +0900

    view prototype

---
+++ src/main/java/com/mouken/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.mouken.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig implements WebSecurityCustomizer {
+
+
+    @Override
+    public void customize(WebSecurity web) {
+        web.ignoring().mvcMatchers("/", "login", "/signup");
+    }
+}
