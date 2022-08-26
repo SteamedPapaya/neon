cd88bd4f4c28c9f3d0db99510efa699aee69579c
ryuu <ryuu.public@gmail.com>
2022-08-26 20:40:31 +0900

    init

---
+++ src/main/java/com/mouken/domain/Account.java
@@ -0,0 +1,31 @@
+package com.mouken.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+}
---
+++ src/test/java/com/mouken/MoukenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoukenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
