89f09f8ca8145f0d741e147bed86d8acb0c23806
ryuu <ryuu.public@gmail.com>
2022-09-30 02:43:13 +0900

    FIX profile

--- src/main/java/com/mouken/infra/mail/HtmlEmailService.java
+++ src/main/java/com/mouken/infra/mail/HtmlEmailService.java
@@ -11,7 +11,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 @Slf4j
-//TODO @Profile("aws")
+@Profile("default")
 @Component
 @RequiredArgsConstructor
 public class HtmlEmailService implements EmailService {
