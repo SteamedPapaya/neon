edf148ccf9c07c68b7e63381c4f4f86b934fb80a
ryuu <ryuu.public@gmail.com>
2022-09-29 12:10:59 +0900

    FIX postresql container dokcer image version

--- src/test/java/com/mouken/infra/AbstractContainerBaseTest.java
+++ src/test/java/com/mouken/infra/AbstractContainerBaseTest.java
@@ -8,7 +8,7 @@ public abstract class AbstractContainerBaseTest {
     static final PostgreSQLContainer POSTGRE_SQL_CONTAINER;
 
     static {
-        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:14.5");
+        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:13.7");
         POSTGRE_SQL_CONTAINER.start();
     }
 
