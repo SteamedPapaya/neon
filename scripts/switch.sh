a4b357e3f1ca05506cd03dd003bcb877cae8d776
ryuu <ryuu.public@gmail.com>
2022-10-02 20:02:31 +0900

    FIX error; nginx reload command

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,5 +11,5 @@ function switch_proxy() {
   echo "> Switching"
   echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
   echo ">> Reload Nginx"
-  sudo service nginx -s reload
+  sudo nginx -s reload
 }
