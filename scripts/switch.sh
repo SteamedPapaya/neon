52aac41b50b8de7436864d4f6107c36b68936d00
ryuu <ryuu.public@gmail.com>
2023-02-17 23:15:35 +0900

    fix: config

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -8,7 +8,7 @@ function switch_proxy() {
   IDLE_PORT=$(find_idle_port)
 
   echo "> Switch to port: $IDLE_PORT"
-  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+  echo 'set \$service_url http://127.0.0.1:${IDLE_PORT};' | sudo tee /etc/nginx/conf.d/service-url.inc
   echo "> Reload Nginx"
   sudo nginx -s reload
 }
